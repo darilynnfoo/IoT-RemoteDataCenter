82dff1fe1b36a76171da95368e420148c7047355
19031353RKJayaVarnikhaa <68110385+19031353RKJayaVarnikhaa@users.noreply.github.com>
2021-05-11 15:32:24 +0800

    codes

---
+++ RGB-Blink/pysense.py
@@ -0,0 +1,33 @@
+import time
+import pycom
+from pycoproc_1 import Pycoproc
+import machine
+
+from LIS2HH12 import LIS2HH12
+from SI7006A20 import SI7006A20
+from LTR329ALS01 import LTR329ALS01
+from MPL3115A2 import MPL3115A2,ALTITUDE,PRESSURE
+
+pycom.heartbeat(False)
+pycom.rgbled(0xFF0000) # Red
+
+py = Pycoproc(Pycoproc.PYSENSE)
+
+pybytes_enabled = False
+if 'pybytes' in globals():
+    if(pybytes.isconnected()):
+        print('Pybytes is connected, sending signals to Pybytes')
+        pybytes_enabled = True
+
+mp = MPL3115A2(py,mode=ALTITUDE) # Returns height in meters. Mode may also be set to PRESSURE, returning a value in Pascals
+print("MPL3115A2 temperature: " + str(mp.temperature()))
+print("Altitude: " + str(mp.altitude()))
+mpp = MPL3115A2(py,mode=PRESSURE) # Returns pressure in Pa. Mode may also be set to ALTITUDE, returning a value in meters
+print("Pressure: " + str(mpp.pressure()))
+
+si = SI7006A20(py)
+print("Temperature: " + str(si.temperature())+ " deg C and Relative Humidity: " + str(si.humidity()) + " %RH")
+print("Dew point: "+ str(si.dew_point()) + " deg C")
+t_ambient = 24.4
+print("Humidity Ambient for " + str(t_ambient) + " deg C is " + str(si.humid_ambient(t_ambient)) + "%RH")
+
