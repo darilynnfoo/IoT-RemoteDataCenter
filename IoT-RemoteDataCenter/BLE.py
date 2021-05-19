4a69d04cb303e393492e2d7693ba6b211f3ae08e
18016183 Darilynn <68259219+darilynnfoo@users.noreply.github.com>
2021-05-19 15:24:59 +0800

    Update BLE.py

--- IoT-RemoteDataCenter/BLE.py
+++ IoT-RemoteDataCenter/BLE.py
@@ -1,3 +1,34 @@
+#done by Darilynn
+#scanning for bluetooth device
+from network import Bluetooth
+
+bluetooth = Bluetooth()
+bluetooth.start_scan(-1)   
+while True:
+    print(bluetooth.get_adv())
+
+#connecting to bluetooth device
+from network import Bluetooth
+import ubinascii
+bluetooth = Bluetooth()
+
+# scan until we can connect to any BLE device around
+bluetooth.start_scan(-1)
+adv = None
+while True:
+    adv = bluetooth.get_adv()
+    if adv:
+        try:
+            bluetooth.connect(adv.mac)
+        except:
+            # start scanning again
+            bluetooth.start_scan(-1)
+            continue
+        break
+print("Connected to device with addr = {}".format(ubinascii.hexlify(adv.mac)))
+
+#done by Jaya
+#connecting to bluetooth device and retrieving datas
 from network import Bluetooth
 import time
 bt = Bluetooth()
@@ -26,3 +57,108 @@ while True:
           break
   else:
       time.sleep(0.050)
+
+#done by Darilynn
+#getting bluetooth device name and data
+import ubinascii
+from network import Bluetooth
+bluetooth = Bluetooth()
+
+bluetooth.start_scan(20)
+while bluetooth.isscanning():
+    adv = bluetooth.get_adv()
+    if adv:
+        # getting complete name
+        print(bluetooth.resolve_adv_data(adv.data, Bluetooth.ADV_NAME_CMPL))
+
+        mfg_data = bluetooth.resolve_adv_data(adv.data, Bluetooth.ADV_MANUFACTURER_DATA)
+
+        if mfg_data:
+            # try to get the manufacturer data 
+            print(ubinascii.hexlify(mfg_data))
+
+#creating a server
+from network import Bluetooth
+from machine import Timer
+
+battery = 100
+update = False
+def conn_cb(chr):
+    events = chr.events()
+    if events & Bluetooth.CLIENT_CONNECTED:
+        print('client connected')
+    elif events & Bluetooth.CLIENT_DISCONNECTED:
+        print('client disconnected')
+        update = False
+
+def chr1_handler(chr, data):
+    global battery
+    global update
+    events = chr.events()
+    print("events: ",events)
+    if events & (Bluetooth.CHAR_READ_EVENT | Bluetooth.CHAR_SUBSCRIBE_EVENT):
+        chr.value(battery)
+        print("transmitted :", battery)
+        if (events & Bluetooth.CHAR_SUBSCRIBE_EVENT):
+            update = True
+
+bluetooth = Bluetooth()
+bluetooth.set_advertisement(name='FiPy 45', manufacturer_data="Pycom", service_uuid=0xec00)
+
+bluetooth.callback(trigger=Bluetooth.CLIENT_CONNECTED | Bluetooth.CLIENT_DISCONNECTED, handler=conn_cb)
+bluetooth.advertise(True)
+
+srv1 = bluetooth.service(uuid=0xec00, isprimary=True,nbr_chars=1)
+
+chr1 = srv1.characteristic(uuid=0xec0e, value='read_from_here') #client reads from here
+
+chr1.callback(trigger=(Bluetooth.CHAR_READ_EVENT | Bluetooth.CHAR_SUBSCRIBE_EVENT), handler=chr1_handler)
+print('Start BLE service')
+def update_handler(update_alarm):
+    global battery
+    global update
+    battery-=1
+    if battery == 1:
+        battery = 100
+    if update:
+        chr1.value(str(battery))
+
+update_alarm = Timer.Alarm(update_handler, 1, periodic=True)
+
+#creating client
+from network import Bluetooth
+from machine import Timer
+
+def char_notify_callback(char, arg):
+    char_value = (char.value())
+    print("New value: {}".format(char_value))
+
+bt = Bluetooth()
+print('Start scanning for BLE services')
+bt.start_scan(-1)
+adv = None
+while(True):
+    adv = bt.get_adv()
+    if adv:
+        try:
+            if bt.resolve_adv_data(adv.data, Bluetooth.ADV_NAME_CMPL)=="FiPy 45":
+                conn = bt.connect(adv.mac)
+                print("Connected to FiPy 45")
+                try:
+                    services = conn.services()
+                    for service in services:
+                        chars = service.characteristics()
+                        for char in chars:
+                            c_uuid = char.uuid()
+                            if c_uuid == 0xec0e:
+                                if (char.properties() & Bluetooth.PROP_NOTIFY):
+                                    char.callback(trigger=Bluetooth.CHAR_NOTIFY_EVENT, handler=char_notify_callback)
+                                    print(c_uuid)
+                                    break
+                except:
+                    continue
+        except:
+            continue
+
+bt.stop_scan()
+bt.disconnect_client()
